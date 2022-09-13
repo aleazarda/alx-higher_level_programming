1ea479c91be381aa862eacf9cbf89a8fc4b5b02a
aleazarda <44554709+aleazarda@users.noreply.github.com>
2022-09-13 23:18:57 +0300

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,76 +1,63 @@
 #!/usr/bin/python3
-"""Create Node class."""
+""" Creating a square class """
 
 
-class Node:
-    """Defining Node linked list."""
+class Square:
+    """
+    Defining a class square
+    """
 
-    def __init__(self, data, next_node=None):
-        """Initialize a new Node.
-        Args:
-            data (int): The data of the new Node.
-            next_node (Node): The next node of the new Node.
-        """
-        self.data = data
-        self.next_node = next_node
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes the data."""
+        self.size = size
+        self.position = position
 
     @property
-    def data(self):
-        """Get/set the data of the Node."""
-        return (self.__data)
+    def size(self):
+        """Returns the size of the square."""
+        return self.__size
 
-    @data.setter
-    def data(self, value):
+    @size.setter
+    def size(self, value):
+        """Sets the size to a value."""
         if not isinstance(value, int):
-            raise TypeError("data must be an integer")
-        self.__data = value
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     @property
-    def next_node(self):
-        """Get/set the next_node of the Node."""
-        return (self.__next_node)
+    def position(self):
+        """Retrieves the position."""
+        return self.__position
 
-    @next_node.setter
-    def next_node(self, value):
-        if not isinstance(value, Node) and value is not None:
-            raise TypeError("next_node must be a Node object")
-        self.__next_node = value
+    @position.setter
+    def position(self, value):
+        """Sets the position to a value."""
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(value[0], int) or not isinstance(value[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
 
-class SinglyLinkedList:
-    """Represent a singly-linked list."""
-
-    def __init__(self):
-        """Initalize a new SinglyLinkedList."""
-        self.__head = None
-
-    def sorted_insert(self, value):
-        """Insert a new Node to the SinglyLinkedList.
-        The node is inserted into the list at the correct
-        ordered numerical position.
-        Args:
-            value (Node): The new Node to insert.
+    def my_print(self):
+        """Prints to stdout the square with the character #,
+        at the position given by the position attribute.
         """
-        new = Node(value)
-        if self.__head is None:
-            new.next_node = None
-            self.__head = new
-        elif self.__head.data > value:
-            new.next_node = self.__head
-            self.__head = new
-        else:
-            tmp = self.__head
-            while (tmp.next_node is not None and
-                    tmp.next_node.data < value):
-                tmp = tmp.next_node
-            new.next_node = tmp.next_node
-            tmp.next_node = new
-
-    def __str__(self):
-        """Define the print() representation of a SinglyLinkedList."""
-        values = []
-        tmp = self.__head
-        while tmp is not None:
-            values.append(str(tmp.data))
-            tmp = tmp.next_node
-        return ('\n'.join(values))
+        if self.__size == 0:
+            print()
+            return
+        for y in range(0, self.__position[1]):
+            print()
+        for i in range(0, self.__size):
+            for x in range(0, self.__position[0]):
+                print(" ", end="")
+            for j in range(0, self.__size):
+                print("#", end="")
+            print()
