f930adb5ade8da51581579496f9b5bd8591c5dc2
aleazarda <44554709+aleazarda@users.noreply.github.com>
2022-11-02 22:17:15 +0300

    Update 15-comedy_only.sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,11 +1,7 @@
--- Lists all comedy shows in the database hbtn_0d_tvshows.
--- Records are ordered by descending show title.
-SELECT t.`title`
-  FROM `tv_shows` AS t
-       INNER JOIN `tv_show_genres` AS s
-       ON t.`id` = s.`show_id`
+-- Uses the hbtn_0d_tvshows database to llists all Comedy shows.
 
-       INNER JOIN `tv_genres` AS g
-       ON g.`id` = s.`genre_id`
-       WHERE g.`name` = "Comedy"
- ORDER BY t.`title`;
+  SELECT s.title
+    FROM (tv_genres g JOIN tv_show_genres sg ON g.id = sg.genre_id)
+    JOIN tv_shows s ON sg.show_id = s.id
+   WHERE g.name = "Comedy"
+ORDER BY s.title ASC;
