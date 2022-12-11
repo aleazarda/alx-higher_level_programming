eb44ef91200d97c141f36717cc14f7b05112b2d3
aleazarda <44554709+aleazarda@users.noreply.github.com>
2022-12-11 22:17:36 +0300

    Update 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
-'use strict';
-let arg = process.argv[2];
-if (isNaN(arg)) {
-  console.log('Not a number');
-} else {
-  console.log('My number: ' + arg);
-}
+const numC = Number(process.argv[2]);
+console.log(isNaN(numC) ? 'Not a number' : 'My number: ' + ~~numC);
