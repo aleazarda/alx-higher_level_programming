37c0d138741d5ff2b00aa26756626926b7128299
aleazarda <44554709+aleazarda@users.noreply.github.com>
2022-12-11 22:22:52 +0300

    Update 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
-'use strict';
-let x = process.argv[2];
-if (isNaN(x)) {
+const times = process.argv[2];
+if (isNaN(times)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < x; i++) {
-    console.log('X'.repeat(x));
+  for (let i = 0; i < times; i++) {
+    let row = '';
+    for (let j = 0; j < times; j++) {
+      row += 'X';
+    }
+    console.log(row);
   }
 }
