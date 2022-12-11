06ac7c1fdb31202ff6cec79f47cecf393d8854fd
aleazarda <44554709+aleazarda@users.noreply.github.com>
2022-12-11 22:32:50 +0300

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
-if (process.argv.length <= 3) {
+
+const myArgs = process.argv.slice(2);
+
+function sortArrayBackwards (a, b) {
+  return (b - a);
+} if (process.argv.length < 4) {
   console.log(0);
 } else {
-  const args = process.argv.map(Number)
-    .slice(2, process.argv.length)
-    .sort((a, b) => a - b);
-  console.log(args[args.length - 2]);
+  myArgs.sort(sortArrayBackwards);
+  console.log(myArgs[1]);
 }
