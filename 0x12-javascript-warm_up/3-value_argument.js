e7f4c45bc0b8f654cf386c11fde7e6639caf0181
aleazarda <44554709+aleazarda@users.noreply.github.com>
2022-12-11 22:14:07 +0300

    Update 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,2 @@
 #!/usr/bin/node
-//'use strict';
-let arg = process.arg[2];
-if (arg === undefined) {
-  console.log('No argument');
-} else {
-  console.log(arg);
-}
+console.log(process.argv[2] === undefined ? 'No argument' : process.argv[2]);
