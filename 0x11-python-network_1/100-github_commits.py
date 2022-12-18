48c941bd675dd8e7da9a3928e44303c8da69ed19
aleazarda <44554709+aleazarda@users.noreply.github.com>
2022-12-18 23:33:34 +0300

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,7 +6,7 @@ sent in as arguments
 if __name__ == '__main__':
     import requests
     from sys import argv
-    r = requests.get('https://api.github.com/repos/{}/{}/commits'
+    r = requests.get('https://developer.github.com/v3/repos/commits/'
                      .format(argv[2], argv[1]))
     commits = r.json()
     for commit in commits[:10]:
