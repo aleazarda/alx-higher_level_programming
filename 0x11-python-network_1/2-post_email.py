9da2ec578835af7f557797b5215ffe75ad780106
aleazarda <44554709+aleazarda@users.noreply.github.com>
2022-12-18 23:13:57 +0300

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""
-takes in a URL and an email, sends a POST request to the passed URL with the
-email as a parameter, and displays the body of the response
-"""
-if __name__ == "__main__":
-    import urllib.parse as parse
-    import urllib.request as request
-    from sys import argv
-    values = {'email': argv[2]}
-    data = parse.urlencode(values).encode('utf-8')
-    req = request.Request(argv[1], data)
-    with request.urlopen(req) as r:
-        print(r.read().decode('utf-8'))
+"""Sends a form data to a URL."""
+import sys
+from urllib import request, parse
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 2:
+        url = sys.argv[1]
+        email = sys.argv[2]
+        form_data = bytes(parse.urlencode([('email', email)]), 'utf-8')
+        with request.urlopen(sys.argv[1], data=form_data) as response:
+            print(response.read().decode('utf-8'))
